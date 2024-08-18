f500108aa0ba389edd88668e2295fbbe15ac87c5
이진 <ctce7226@gmail.com>
2024-08-18 22:41:18 +0900

    chore: 서버 프로젝트 초기설정

---
+++ src/main/java/hanium/server/i_luv_book/ILuvBookApplication.java
@@ -0,0 +1,13 @@
+package hanium.server.i_luv_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILuvBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILuvBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanium/server/i_luv_book/ILuvBookApplicationTests.java
@@ -0,0 +1,13 @@
+package hanium.server.i_luv_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILuvBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
