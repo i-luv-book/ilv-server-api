aa81c6712894351fd48caf985642a9f1603d0df1
이진 <ctce7226@gmail.com>
2024-08-21 23:19:25 +0900

    feat: File I/O 과정에서 발생하는 예외 Enum Class 추가

---
+++ src/main/java/hanium/server/i_luv_book/exception/code/FileCode.java
@@ -0,0 +1,23 @@
+package hanium.server.i_luv_book.exception.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author ijin
+ */
+@Getter
+@RequiredArgsConstructor
+public enum FileCode {
+    EMPTY("F-01"),
+    NON_EXISTENT_EXTENSION("F-02"),
+    INVALID_EXTENSION("F-03"),
+    LIMITED_SIZE("F-04"),
+    FAILED_IO("F-05"),
+    FAILED_PUT_OBJECT("F-06"),
+    FAILED_DELETE_OBJECT("F-07"),
+    ;
+
+    private final String code;
+}
+
