86d848ca5a629c6975765fe1562e270927ac7339
이진 <ctce7226@gmail.com>
2024-09-10 13:27:30 +0900

    refactor: 자식 추가 및 검증, 삭제 API에 Spring Security 적용

--- src/main/java/hanium/server/i_luv_book/domain/user/presentation/UserController.java
+++ src/main/java/hanium/server/i_luv_book/domain/user/presentation/UserController.java
@@ -3,8 +3,10 @@ package hanium.server.i_luv_book.domain.user.presentation;
 import hanium.server.i_luv_book.domain.user.application.UserCommandService;
 import hanium.server.i_luv_book.domain.user.presentation.dto.UserDtoMapper;
 import hanium.server.i_luv_book.domain.user.presentation.dto.request.ChildCreateDto;
+import hanium.server.i_luv_book.global.security.authentication.userdetails.JwtUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,31 +14,42 @@ import org.springframework.web.multipart.MultipartFile;
  * @author ijin
  */
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserDtoMapper mapper;
     private final UserCommandService userCommandService;
 
-    @PostMapping("/child/can-add")
-    public void canAddChildAccount(@RequestParam(value = "parentId") long parentId) {
+    // 부모 APIs
+    @PostMapping("/parent/child/can-add")
+    public void canAddChildAccount(@AuthenticationPrincipal JwtUserDetails userDetails) {
+        Long parentId = userDetails.getUserId();
         userCommandService.checkChildAdditionPossible(parentId);
     }
 
-    @PostMapping("/child")
-    public void registerChild(@RequestParam(value = "parentId") long parentId, @RequestPart(value = "childCreateDto") @Valid ChildCreateDto childCreateDto,
-                              @RequestPart(value = "image", required = false) MultipartFile image) {
+    @PostMapping("/parent/child")
+    public void registerChild(@AuthenticationPrincipal JwtUserDetails userDetails,
+                              @RequestPart(value = "childCreateDto") @Valid ChildCreateDto childCreateDto, @RequestPart(value = "image", required = false) MultipartFile image) {
+        Long parentId = userDetails.getUserId();
         userCommandService.registerChild(mapper.toCommand(childCreateDto, parentId), image);
     }
 
-    @DeleteMapping("/child")
-    public void deleteChild(@RequestParam(value = "parentId") long parentId, @RequestParam(value = "nickname") String nickname) {
+    @DeleteMapping("/parent/child")
+    public void deleteChild(@AuthenticationPrincipal JwtUserDetails userDetails, @RequestParam(value = "nickname") String nickname) {
+        Long parentId = userDetails.getUserId();
         userCommandService.deleteChild(parentId, nickname);
     }
 
+    @GetMapping("/parent/children")
+    public void getChildren(@AuthenticationPrincipal JwtUserDetails userDetails) {
+        Long parentId = userDetails.getUserId();
+    }
+
+    // 자식 APIs
     @PostMapping("/child/badge")
-    public void registerBadge(@RequestParam(value = "childId") long childId, @RequestParam(value = "badgeId") long badgeId) {
+    public void registerBadge(@AuthenticationPrincipal JwtUserDetails userDetails, @RequestParam(value = "badgeId") long badgeId) {
+        Long childId = userDetails.getUserId();
         userCommandService.grantBadge(childId, badgeId);
     }
 }
